e7bd89f74ced40a8325e8cf36ec7564df48fe10d
Daniele <bambudio@libero.it>
2021-05-03 15:36:55 +0200

    Frase censurata

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    $par = $_GET["paragrafo"];
+    $badword = 'merda';
+    $parCens = str_replace($badword, '***', $par);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP badwords</title>
+    </head>
+    <body>
+        <h1>Frase censurata: <?php echo $parCens?></h1>
+    </body>
+</html>
